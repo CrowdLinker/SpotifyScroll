81cb9d529ac7bf3a7d1355dd2c2ad59b528a9b74
andy <andyboythekid@gmail.com>
2020-02-18 11:01:32 -0700

    step 1: setup layout and components

--- App.js
+++ App.js
@@ -1,114 +1,103 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {View, Animated, SafeAreaView, TouchableOpacity} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+function PlaylistProfile() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <SafeAreaView style={{flex: 1}}>
+      <Header />
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+      <Animated.ScrollView style={{flex: 1}}>
+        <SearchPlaylists />
+
+        <PlaylistHero>
+          <View style={{height: 180, width: 180, backgroundColor: 'gray'}} />
+        </PlaylistHero>
+
+        <PlaylistItems>
+          <ShufflePlayButton />
+        </PlaylistItems>
+      </Animated.ScrollView>
+    </SafeAreaView>
+  );
+}
+
+// the following components are meant to be containers for content
+// ordinarily, the height of a view will be determined by its content (it's intrinsic height)
+// we're using preset heights here to get the approximate layout first
+
+// any calculations involving the intrinsic height of a view (in later steps) will be calculated using the onLayout prop -- if this sounds confusing, it will be explained in a later step - dont worry about it for now
+
+const HEADER_HEIGHT = 40;
+
+function Header({children}: any) {
+  return (
+    <View style={{height: HEADER_HEIGHT, backgroundColor: 'aquamarine'}}>
+      {children}
+    </View>
+  );
+}
+
+const SEARCH_PLAYLISTS_HEIGHT = 50;
+
+function SearchPlaylists({children}: any) {
+  return (
+    <View style={{height: SEARCH_PLAYLISTS_HEIGHT, backgroundColor: 'coral'}}>
+      {children}
+    </View>
+  );
+}
+
+const PLAYLIST_HERO_HEIGHT = 300;
+
+function PlaylistHero({children}: any) {
+  return (
+    <View
+      style={{
+        padding: 10,
+        height: PLAYLIST_HERO_HEIGHT,
+        alignItems: 'center',
+      }}>
+      {children}
+    </View>
+  );
+}
+
+function PlaylistItems({children}: any) {
+  return (
+    <View style={{height: 1200, backgroundColor: 'rebeccapurple'}}>
+      {children}
+    </View>
+  );
+}
+
+const SHUFFLE_PLAY_BUTTON_HEIGHT = 60;
+// offset is used to move the button slightly outside its container view
+// this gives it the effect of sitting halfway between the hero section and the playlist items section
+const SHUFFLE_PLAY_BUTTON_OFFSET = SHUFFLE_PLAY_BUTTON_HEIGHT / 2;
+
+function ShufflePlayButton({children}: any) {
+  return (
+    <TouchableOpacity
+      style={{
+        transform: [
+          {
+            // shifts the button up halway
+            translateY: -SHUFFLE_PLAY_BUTTON_OFFSET,
+          },
+        ],
+        alignSelf: 'center',
+        height: SHUFFLE_PLAY_BUTTON_HEIGHT,
+        width: 220,
+        borderRadius: 30,
+        backgroundColor: 'springgreen',
+      }}>
+      {children}
+    </TouchableOpacity>
+  );
+}
+
+function App() {
+  return <PlaylistProfile />;
+}
 
 export default App;
